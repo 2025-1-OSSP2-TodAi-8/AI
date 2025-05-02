a4a498e1d1d2e18eb96026ffb664d146599daf45
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-05-02 16:57:34 +0900

    Fix: URL 수정

--- emotion/urls.py
+++ emotion/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("day/", views.emotions_day),
-    path("month/", views.emotions_month),
-    path("year/", views.emotions_year),
+    path("day", views.emotions_day),
+    path("month", views.emotions_month),
+    path("year", views.emotions_year),
 ]
