ccdfda285ef939fa57cf06d94e9064ed1f6b44d9
JAEHYUK LEE <107346845+jaehyuk2-lee@users.noreply.github.com>
2025-05-07 16:42:05 +0900

    feat: diary 기능 추가

--- config/settings.py
+++ config/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "people.apps.PeopleConfig",
-    "emotion.apps.EmotionConfig",
+    "people",
+    "emotion",
+    "diary",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
